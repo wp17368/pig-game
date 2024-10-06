b065208030b4480467a2405e6d2d54bc3c5f6cbd
wasp <margi4@op.pl>
2024-10-06 20:38:23 +0200

    Add functionalities for game script

--- script.js
+++ script.js
@@ -1,1 +1,84 @@
 'use strict';
+const startNewGameButton = document.querySelector('.btn--new');
+const rollDiceButton = document.querySelector('.btn--roll');
+const holdButton = document.querySelector('.btn--hold');
+const diceImage = document.querySelector('.dice');
+const playerZeroSection = document.querySelector('.player--0');
+const playerOneSection = document.querySelector('.player--1');
+
+function createNewPlayer(name) {
+  return {
+    name: name,
+    score: 0,
+    currentScore: 0,
+    active: true,
+  };
+}
+
+let diceRollResult;
+let players = [createNewPlayer('playerZero'), createNewPlayer('playerOne')];
+function addDomElementsToPlayersObjects() {
+  for (let i = 0; i < players.length; i++) {
+    players[i].sectionDomElement = document.querySelector(`.player--${i}`);
+    players[i].currentScoreDomElement = document.querySelector(`#score--${i}`);
+    players[i].scoreDomElement = document.querySelector(`#current--${i}`);
+  }
+}
+addDomElementsToPlayersObjects();
+console.log(players[1]);
+
+function getActivePlayer() {
+  if (players[0].active) {
+    return players[0];
+  } else {
+    return players[1];
+  }
+}
+function toggleActivePlayer() {
+  if (players[0].active) {
+    players[0].active = false;
+    players[1].active = true;
+  } else {
+    players[1].active = false;
+    players[0].active = true;
+  }
+}
+function generateRandomNumber(minNumber, maxNumber) {
+  return Math.floor(Math.random() * maxNumber + minNumber);
+}
+// startNewGameButton.addEventListener('click');
+rollDiceButton.addEventListener('click', rollTheDice);
+holdButton.addEventListener('click', holdCurrentScore);
+
+function rollTheDice() {
+  diceRollResult = generateRandomNumber(1, 6);
+  displayDiceNumber(diceRollResult);
+  changeCurrentScore(getActivePlayer());
+  displayCurrentScore(getActivePlayer());
+  if (getActivePlayer().currentScore === 0) {
+    toggleActivePlayer();
+  }
+}
+function displayDiceNumber(diceRollResult) {
+  diceImage.src = `dice-${diceRollResult}.png`;
+}
+function changeCurrentScore(activePlayer) {
+  if (diceRollResult != 1) {
+    activePlayer.currentScore += diceRollResult;
+  } else {
+    activePlayer.currentScore = 0;
+  }
+}
+function displayCurrentScore(activePlayer) {
+  activePlayer.currentScoreDomElement.innerHTML = `${activePlayer.currentScore}`;
+}
+function holdCurrentScore() {
+  getActivePlayer().score += getActivePlayer().currentScore;
+  displayNewScore(getActivePlayer());
+  getActivePlayer().currentScore = 0;
+  displayCurrentScore(getActivePlayer());
+  toggleActivePlayer();
+}
+function displayNewScore(activePlayer) {
+  activePlayer.scoreDomElement.innerText = activePlayer.score;
+}
